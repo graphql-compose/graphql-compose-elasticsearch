5fb0ae54846f2394d261ebca104985d2e5f4bfc5
Ajmal Sali <ajmalmsali@users.noreply.github.com>
2022-07-17 14:14:34 +0530

    Update Aggs - replace __ with [.dot] recursively
    Nested keys with __ are not transformed

--- src/elasticDSL/Aggs/Aggs.ts
+++ src/elasticDSL/Aggs/Aggs.ts
@@ -52,8 +52,12 @@ export function convertAggsRules(rules: GqlAggRules): ElasticAggsRulesT {
   Object.keys(rules).forEach((key) => {
     if (key === 'aggs' && rules.aggs) {
       result.aggs = convertAggsBlocks(rules.aggs);
-    } else {
-      result[key] = rules[key];
+    } else if(Array.isArray(rules[key])) {
+      result[key.replace(/__/g, '.')] = rules[key].map(rule => convertAggsRules(rule))
+    } else if(typeof rules[key] === 'object') {
+      result[key.replace(/__/g, '.')] = convertAggsRules(rules[key]);
+    }else {
+      result[key.replace(/__/g, '.')] = rules[key];
     }
   });
   return result;
